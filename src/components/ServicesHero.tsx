f52f9caff5e0bab58a6c2cfb4734d2ceafd8db58
Peter Farkas <pfarkas@yext.com>
2023-09-28 11:15:16 -0400

    start service template

---
+++ src/components/ServicesHero.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+export interface BannerProps {
+    pageTitle?: string;
+    imageUrl?: string;
+  }
+
+const ServicesHero = ({pageTitle, imageUrl}: BannerProps) => {
+
+  return (
+    <div className="bg-white">
+      <div className="relative">
+        <div className="mx-auto max-w-7xl">
+          <div className="relative z-10 lg:w-full lg:max-w-2xl">
+            <svg
+              className="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
+            >
+              <polygon points="0,0 90,0 50,100 0,100" />
+            </svg>
+
+            <div className="relative px-6 py-24 sm:py-24 lg:px-8 lg:py-24 lg:pr-0">
+              <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+                <div className="hidden sm:mb-10 sm:flex">
+                  <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                    Anim aute id magna aliqua ad ad non deserunt sunt.{' '}
+                    <a href="#" className="whitespace-nowrap font-semibold text-indigo-600">
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      Read more <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </div>
+                </div>
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                  {pageTitle}
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
+                  amet fugiat veniam occaecat fugiat aliqua.
+                </p>
+                <div className="mt-10 flex items-center gap-x-6">
+                  <a
+                    href="#"
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Get started
+                  </a>
+                  <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+          <img
+            className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full"
+            src={imageUrl}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ServicesHero;
---
+++ src/templates/service.tsx
@@ -0,0 +1,46 @@
+import { GetPath, TemplateConfig, TemplateProps } from "@yext/pages";
+import PageLayout from "../components/common/PageLayout";
+import "@fontsource/lato/100.css";
+import "@fontsource/lato/300.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
+import "@fontsource/lato/900.css";
+import "@fontsource/lato/100-italic.css";
+import "@fontsource/lato/300-italic.css";
+import "@fontsource/lato/400-italic.css";
+import "@fontsource/lato/700-italic.css";
+import "@fontsource/lato/900-italic.css";
+import "../index.css";
+import HeroSection from "../components/home/HeroSection";
+import ServicesHero from "../components/ServicesHero";
+export const config: TemplateConfig = {
+  stream: {
+    $id: "services",
+    localization: { locales: ["en"], primary: false },
+    filter: {
+      entityTypes: ["fins_service"],
+    },
+    fields: ["name", "id", "slug", "fins_servicesImage"],
+  },
+};
+export const getPath: GetPath<TemplateProps> = ({ document }) => {
+  return document.slug ?? document.id.toString();
+};
+
+export default function Service({ document, __meta }: TemplateProps) {
+  let pageTitle = `${document.name} at Capital Wealth`;
+  if (document.name === "Mortgage") {
+    pageTitle = "Mortages at Capital Wealth"
+  } else if (document.name === "Retail Banking") {
+    pageTitle = "Personal Banking with Capital";
+  }
+
+  return (
+    <PageLayout _site={document._site}>
+      <ServicesHero
+        pageTitle={pageTitle}
+        imageUrl={document.fins_servicesImage.url}
+      />
+    </PageLayout>
+  );
+}
